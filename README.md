da379ef252e70580c5226290b97e5832a39b0f53
Alexander Thiemann <mail@athiemann.net>
2020-12-20 20:26:56 -0800

    add rate limiting key

--- README.md
+++ README.md
@@ -6,12 +6,18 @@ A simple in-memory rate-limiter library.
 
 ``` haskell
 import Control.RateLimiter
+import qualified Data.Vector as V
 
 main :: IO
 main =
-  do limiter <- newRateLimiter $ RateLimitConfig (RollingWindow 60) 200
+  -- one rate limiter can have multiple rules
+  do limiter <- 
+        newRateLimiter $ V.fromList
+        [ RateLimitConfig (RollingWindow 60) 200 -- 200 per second
+        , RateLimitConfig (RollingWindow 3600) 400 -- 400 per hour
+        ]
      let myRateLimitedFunction =
-           do isLimited <- isRateLimited limiter
+           do isLimited <- isRateLimited () limiter
               if isLimitd then pure Nothing else Just <$> someExpensiveWork
      -- ... use myRateLimitedFunction
 ```
--- src/Control/RateLimiter.hs
+++ src/Control/RateLimiter.hs
@@ -33,50 +33,60 @@ data RateLimitConfig
     , rlc_maximum :: !Int
     }
 
-data RateLimiterImpl
+data Entry k
+  = Entry
+  { eTime :: !UTCTime
+  , eKey :: !k
+  }
+
+data RateLimiterImpl k
     = RateLimiterImpl
     { rl_config :: !RateLimitConfig
-    , rl_state :: !(IORef (Seq.Seq UTCTime))
+    , rl_state :: !(IORef (Seq.Seq (Entry k)))
     }
 
-newtype RateLimiter
-    = RateLimiter { _unRateLimiter :: V.Vector RateLimiterImpl }
+newtype RateLimiter k
+    = RateLimiter { _unRateLimiter :: V.Vector (RateLimiterImpl k) }
 
-newRateLimiter :: V.Vector RateLimitConfig -> IO RateLimiter
+-- | Create a new rate limiter with a list of configurations
+newRateLimiter :: V.Vector RateLimitConfig -> IO (RateLimiter k)
 newRateLimiter cfgs =
     liftM RateLimiter $ forM cfgs $ \cfg ->
     do ref <- newIORef mempty
        pure $ RateLimiterImpl { rl_config = cfg, rl_state = ref }
 
-isRateLimited :: MonadIO m => RateLimiter -> m Bool
-isRateLimited (RateLimiter rls) =
-    anyM isRateLimitedImpl $ V.toList rls
+-- | Check if a given key is rate limited. Use `()` if you don't need multiple keys
+isRateLimited :: (Eq k, MonadIO m) => k -> RateLimiter k -> m Bool
+isRateLimited key (RateLimiter rls) =
+    anyM (isRateLimitedImpl key) $ V.toList rls
 
-isRateLimitedImpl :: MonadIO m => RateLimiterImpl -> m Bool
-isRateLimitedImpl rl =
+isRateLimitedImpl :: Eq k => MonadIO m => k -> RateLimiterImpl k -> m Bool
+isRateLimitedImpl key rl =
     do now <- liftIO getCurrentTime
        (_, isLimited) <-
            liftIO $
            atomicModifyIORef' (rl_state rl) $ \xs ->
            do let (xs', bucketSize) =
-                      currentBucketSize now (rlc_mode $ rl_config rl) xs
+                      currentBucketSize now key (rlc_mode $ rl_config rl) xs
               if bucketSize < rlc_maximum (rl_config rl)
-                 then (xs' Seq.|> now, (bucketSize, False))
+                 then (xs' Seq.|> Entry now key, (bucketSize, False))
                  else (xs', (bucketSize, True))
        pure isLimited
 
-withRetryRateLimiter :: MonadIO m => RateLimiter -> m a -> m a
-withRetryRateLimiter rl action =
-    do limited <- isRateLimited rl
+-- | Retry action if rate limited after 1 second
+withRetryRateLimiter :: Eq k => MonadIO m => k -> RateLimiter k -> m a -> m a
+withRetryRateLimiter key rl action =
+    do limited <- isRateLimited key rl
        if not limited
           then action
           else do liftIO $ sleepTS (seconds 1)
-                  withRetryRateLimiter rl action
+                  withRetryRateLimiter key rl action
 
 currentBucketSize ::
-    UTCTime -> RateLimitMode -> Seq.Seq UTCTime -> (Seq.Seq UTCTime, Int)
-currentBucketSize now rlm times =
-    let timeOfDay = timeToTimeOfDay (utctDayTime now)
+    Eq k => UTCTime -> k -> RateLimitMode -> Seq.Seq (Entry k) -> (Seq.Seq (Entry k), Int)
+currentBucketSize now key rlm times =
+    let timeOfDay =
+          timeToTimeOfDay (utctDayTime now)
         takeUntil =
             case rlm of
               RollingWindow dt ->
@@ -91,5 +101,7 @@ currentBucketSize now rlm times =
                         now { utctDayTime = fromInteger $ truncate (utctDayTime now) }
                     WsDay ->
                         now { utctDayTime = 0 }
-        newSeq = Seq.takeWhileR (>= takeUntil) times
-    in (newSeq, Seq.length newSeq)
+        newSeq = Seq.takeWhileR (\el -> eTime el >= takeUntil) times
+    in ( newSeq
+       , Seq.length $ Seq.filter (\el -> eKey el == key) newSeq
+       )
